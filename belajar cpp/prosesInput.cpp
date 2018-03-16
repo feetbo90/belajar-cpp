88097d0563db852a2a07adb9fe2ff603236fccc1
Unknown <feetbonike90@gmail.com>
2018-03-16 18:00:24 +0700

    input aritmatika

--- belajar cpp/prosesInput.cpp
+++ belajar cpp/prosesInput.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
-  int nilai;
-  cin>>nilai;
-  cout<<nilai;
+  int bilangan1, bilangan2;
+  cout<<"Masukkan bilangan pertama : "<<endl;
+  cin>>bilangan1;
+  cout<<"Masukkan bilangan kedua : "<<endl;
+  cin>>bilangan2;
+
+  cout<<"hasil penjumlahannya : "<<(bilangan1 + bilangan2)<<endl;
+  cout<<"hasil pengurangannya : "<<(bilangan1 - bilangan2)<<endl;
+  cout<<"hasil pembagiannya : "<<((float)bilangan1 / (float) bilangan2)<<endl;
+  cout<<"hasil perkaliannya : "<<(bilangan1 * bilangan2)<<endl;
+  cout<<"hasil modulo : "<<(bilangan1 % bilangan2)<<endl;
+
 }
