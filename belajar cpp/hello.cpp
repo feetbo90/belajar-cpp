7560061c52811a1f4538adcdac62ddaf775b385e
Unknown <feetbonike90@gmail.com>
2018-03-16 17:37:32 +0700

    hello dan variabel

---
+++ belajar cpp/hello.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+// deklarasi variabel
+// tipe data
+// -10 s/d 10 int
+// -1.1 s/d 11.1 float
+// double
+// char "9" "a"
+// string "saya" "saya sedang makan"
+
+// tipe_data nama_variabel = nilai
+// contoh
+// int bilangan1 = 10;
+// int bilangan1 ;
+// bilangan1 = 10;
+
+
+
+int main(){
+
+  cout<<"\t Hello World \n "<<endl;
+
+
+}
---
+++ belajar cpp/variabel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int bilangan1 = 10;
+  int bilangan2 = 20;
+
+  int hasilTambah = bilangan1 + bilangan2;
+  cout<<"maka hasil penjumlahannya "<<bilangan1 <<" + "<<
+  bilangan2<< " = "<<hasilTambah<<endl;
+
+}
+
+
+// operator aritmatika
+// + , - , / , *, %
